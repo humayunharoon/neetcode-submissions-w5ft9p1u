5239b32a1585ea3123be60128c332d3a96b57326
humayunharoon <humayunharoon@gmail.com>
2026-04-21 21:15:28 +0800

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.swift
@@ -0,0 +1,36 @@
+class Solution {
+    func isPalindrome(_ s: String) -> Bool {
+
+        let filtered = Array(s.lowercased().filter { $0.isASCII && ($0.isNumber || $0.isLetter) })
+        let isEven = filtered.count % 2
+
+        if isEven == 0 {
+            var left = 0
+            var right = filtered.count - 1
+
+            while left < right {
+                if filtered[left] == filtered[right] {
+                    left += 1
+                    right -= 1
+                } else {
+                    return false
+                }
+            }
+        } else {
+            var left = 0
+            var right = filtered.count - 1
+            let center = filtered.count / 2
+
+            while left < center {
+                if filtered[left] == filtered[right] {
+                    left += 1
+                    right -= 1
+                } else {
+                    return false
+                }
+            }
+        }
+
+        return true
+    }
+}
