14c6eb9fd3791bdd21d4f6019f22b2779a830035
humayunharoon <humayunharoon@gmail.com>
2026-05-24 11:49:03 +0800

    Add: level-order-traversal-of-binary-tree - submission-1

---
+++ Data Structures & Algorithms/level-order-traversal-of-binary-tree/submission-1.swift
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     var val: Int
+ *     var left: TreeNode?
+ *     var right: TreeNode?
+ *     init(_ val: Int) {
+ *         self.val = val
+ *         self.left = nil
+ *         self.right = nil
+ *     }
+ * }
+ */
+
+class Solution {
+    func levelOrder(_ root: TreeNode?) -> [[Int]] {
+
+        var queue: [TreeNode?] = []
+        var output: [[Int]] = []
+
+        if root != nil {
+            queue.append(root)
+        } else {
+            return output
+        }
+
+        while !queue.isEmpty {
+            var array: [Int] = []
+            var len = queue.count
+
+            for _ in 0..<len {
+                let node = queue.removeFirst()
+                array.append(node!.val)
+                if node?.left != nil {
+                    queue.append(node?.left)
+                }
+
+                if node?.right != nil {
+                    queue.append(node?.right)
+                }
+            }
+
+            output.append(array)
+        }
+
+        return output
+    }
+}
