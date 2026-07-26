77bf5d14227ea13e263180c0475fb391f2634b10
humayunharoon <humayunharoon@gmail.com>
2026-07-26 18:08:01 +0800

    Add: design-twitter-feed - submission-0

---
+++ Data Structures & Algorithms/design-twitter-feed/submission-0.swift
@@ -0,0 +1,42 @@
+class Twitter {
+
+    var users: [Int: Set<Int>]
+    var tweets: [(Int, Int)]
+
+    init() {
+        users = [:]
+        tweets = []
+    }
+
+    func postTweet(_ userId: Int, _ tweetId: Int) {
+        tweets.append((userId, tweetId))
+    }
+
+    func getNewsFeed(_ userId: Int) -> [Int] {
+        var newsfeed: [Int] = []
+        var count = tweets.count - 1
+        var validSet = Set<Int>()
+        validSet.insert(userId)
+
+        if let following = users[userId] {
+            validSet.formUnion(following)
+        } 
+
+        while newsfeed.count < 10 && count >= 0 {
+            if validSet.contains(tweets[count].0) {
+                newsfeed.append(tweets[count].1)
+            }
+            count -= 1
+        } 
+
+        return newsfeed
+    }
+
+    func follow(_ followerId: Int, _ followeeId: Int) {
+        users[followerId, default:[]].insert(followeeId)
+    }
+
+    func unfollow(_ followerId: Int, _ followeeId: Int) {
+        users[followerId, default:[]].remove(followeeId)
+    }
+}
